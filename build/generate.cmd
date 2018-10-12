34fe211ef8b7e4f940d06b31b25ae20bbfec6a39
Roland Aoun <roland.aoun@murex.com>
2018-10-12 15:50:14 +0300

    Removing release version, bug hiding header includes in timehandlers

--- build/generate.cmd
+++ build/generate.cmd
@@ -1,7 +1,7 @@
-mkdir Release
-cd Release
-cmake ..\.. -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017 Win64"
-cd ..
+REM mkdir Release
+REM cd Release
+REM cmake ..\.. -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017 Win64"
+REM cd ..
 
 mkdir Debug
 cd Debug
